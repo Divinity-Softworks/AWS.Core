f5da405e575cd345f4b401d76dca9e230d0a326f
Divinity Softworks <divinitysoftworks@gmail.com>
2024-12-28 11:16:32 +0100

    Updated a summary.

--- DivinitySoftworks.AWS.Core.Web.Functions/Contracts/HealthCheckResponse.cs
+++ DivinitySoftworks.AWS.Core.Web.Functions/Contracts/HealthCheckResponse.cs
@@ -7,7 +7,18 @@ namespace DivinitySoftworks.AWS.Core.Web.Functions.Contracts;
 /// </summary>
 public record HealthCheckResponse {
     /// <summary>
-    /// Gets or sets the status of the health check.
+    /// Gets the health status of the system or component.
     /// </summary>
+    /// <remarks>
+    /// This property provides a read-only representation of the current health status,
+    /// which is defined by the <see cref="HealthStatus"/> enumeration.
+    /// The possible values are:
+    /// <list type="bullet">
+    ///     <item><description><see cref="HealthStatus.Healthy"/> - The system or component is healthy and operational.</description></item>
+    ///     <item><description><see cref="HealthStatus.Degraded"/> - The system or component is degraded but still operational.</description></item>
+    ///     <item><description><see cref="HealthStatus.Unhealthy"/> - The system or component is unhealthy and not operational.</description></item>
+    /// </list>
+    /// </remarks>
     public HealthStatus Status { get; init; }
 }
+
